3c46108993b5fa4b28f074f4888701275dc57a92
JJavier98 <jota.jota.98@hotmail.com>
2018-05-06 17:08:24 +0200

    domingo 6/4/2018 17.08

--- SEMINARIOS/Seminario_3/ejer3/scheduled-clause.c
+++ SEMINARIOS/Seminario_3/ejer3/scheduled-clause.c
@@ -9,6 +9,9 @@
 int main(int argc, char **argv)
 {
 	int i, n=200,chunk,a[n],suma=0;
+	omp_sched_t kind;
+	int modifier;
+	char respuesta[2];
 	if(argc < 3)
 	{
 		fprintf(stderr,"\nFalta iteraciones o chunk \n");
@@ -26,7 +29,34 @@ int main(int argc, char **argv)
 		suma = suma + a[i];
 		printf(" thread %d suma a[%d]=%d suma=%d \n",
 		omp_get_thread_num(),i,a[i],suma);
+		if(i == 0)
+		{
+			if(omp_get_dynamic())
+			{
+				respuesta[0] = 's';
+				respuesta[1] = 'i';
+			}
+			else
+			{
+				respuesta[0] = 'n';
+				respuesta[1] = 'o';
+			}
+			printf("\n\n");
+			printf("Puede alterar el nº de threads dinámicamente? %s\n", respuesta);
+			printf("Número actual de hebras: %d\n", omp_get_max_threads());
+			printf("Limite de hebras: %d\n", omp_get_thread_limit());
+			omp_get_schedule(&kind, &modifier);
+			printf("Tipo de distribución de hebras %d. Modificador: %d\n",kind, modifier);
+			printf("\n\n");
+		}
 	}
 
 	printf("Fuera de 'parallel for' suma=%d\n",suma);
+	printf("\n\n");
+	printf("Puede alterar el nº de threads dinámicamente? %s\n", respuesta);
+	printf("Número actual de hebras: %d\n", omp_get_max_threads());
+	printf("Limite de hebras: %d\n", omp_get_thread_limit());
+	omp_get_schedule(&kind, &modifier);
+	printf("Tipo de distribución de hebras %d. Modificador: %d\n",kind, modifier);
+	printf("\n\n");
 }
